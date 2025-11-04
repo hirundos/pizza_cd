21528c1f3941ec956c71d6b40c13ef2c12787af3
hirundos <dkdus@gmail.com>
2025-11-04 18:38:49 +0900

    dag update

--- dags/etl_k8s.py
+++ dags/etl_k8s.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from kubernetes.client import models as k8s
 from datetime import datetime
 
 default_args = {
@@ -8,6 +9,20 @@ default_args = {
     "start_date": datetime(2025, 1, 1),
     "retries": 1,
 }
+
+k8s_volume_mount = k8s.V1VolumeMount(
+    name='dags-volume',         
+    mount_path='/opt/dags',    
+    read_only=True             
+)
+
+k8s_volume = k8s.V1Volume(
+    name='dags-volume',         
+    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(
+        claim_name='airflow-dags-pvc'
+    )
+)
+
 
 with DAG(
     dag_id="spark_etl_pipeline_k8s",
@@ -25,6 +40,8 @@ with DAG(
         arguments=["kubectl apply -f /opt/dags/spark-apps/bronze.yaml"],
         get_logs=True,
         service_account_name="spark-app-runner",
+        volumes=[k8s_volume],
+        volume_mounts=[k8s_volume_mount]
     )
 
     silver = KubernetesPodOperator(
@@ -36,6 +53,8 @@ with DAG(
         arguments=["kubectl apply -f /opt/dags/spark-apps/silver.yaml"],
         get_logs=True,
         service_account_name="spark-app-runner",
+        volumes=[k8s_volume],
+        volume_mounts=[k8s_volume_mount]
     )
 
     gold = KubernetesPodOperator(
@@ -47,6 +66,8 @@ with DAG(
         arguments=["kubectl apply -f /opt/dags/spark-apps/gold.yaml"],
         get_logs=True,
         service_account_name="spark-app-runner",
+        volumes=[k8s_volume],
+        volume_mounts=[k8s_volume_mount]    
     )
 
     bronze >> silver >> gold
